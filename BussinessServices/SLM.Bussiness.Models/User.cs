cd4d5ae51f38d36976b20e906c8ce1d2397476e9
anum zeb <anum.z95@simplilearn.com>
2023-01-31 22:36:09 +0500

    10:35 PM

--- BussinessServices/SLM.Bussiness.Models/User.cs
+++ BussinessServices/SLM.Bussiness.Models/User.cs
@@ -12,6 +12,8 @@ namespace SLM.Bussiness.Models
 
         public string Name { get; set; }
 
+        public string Image { get; set; }
+
         public string Email { get; set; }
 
         public string Password { get; set; }

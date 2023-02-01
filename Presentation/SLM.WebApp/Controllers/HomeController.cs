e36c3bdc24fe7e6fb578768cd5d54efa3171b82c
anum zeb <anum.z95@simplilearn.com>
2023-02-01 06:35:30 +0500

    6:35 AM

--- Presentation/SLM.WebApp/Controllers/HomeController.cs
+++ Presentation/SLM.WebApp/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace SLM.WebApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }

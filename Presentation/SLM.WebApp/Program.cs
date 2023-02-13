8ed0ae8f0a7dc4c1410c3fd054ead38d18704a96
anum zeb <anum.z95@simplilearn.com>
2023-02-14 03:46:45 +0500

    3:46 AM

--- Presentation/SLM.WebApp/Program.cs
+++ Presentation/SLM.WebApp/Program.cs
@@ -1,4 +1,5 @@
 
+using Microsoft.AspNetCore.Identity;
 using SLM.DependencyInjection;
 
 namespace SLM.WebApp
@@ -7,41 +8,55 @@ namespace SLM.WebApp
 	{
 		public static void Main(string[] args)
 		{
-			var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
-			builder.Services.AddControllersWithViews();
+            var builder = WebApplication.CreateBuilder(args);
+            //            var connectionString = builder.Configuration.GetConnectionString("DbConnection") ?? throw new InvalidOperationException("Connection string 'DbConnection' not found.");
+
+            //            builder.Services.AddDbContext<DbContext>(options =>
+            // options.UseSqlServer(connectionString));
+
+            //            builder.Services.AddDefaultIdentity<BaseEntity>(options => options.SignIn.RequireConfirmedAccount = true)
+            //.AddEntityFrameworkStores<DbContext>();
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages();
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>();
 
 
-			//All application DI configyrations
-			builder.Services.AppDISetup(builder.Configuration);
+
+
+            //All application DI configyrations
+            builder.Services.AppDISetup(builder.Configuration);
 
 
 
-			var app = builder.Build();
+            var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
-			if (!app.Environment.IsDevelopment())
-			{
-				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-			app.UseRouting();
-			app.UseAuthentication();
-			app.UseAuthorization();
+            app.UseRouting();
 
-			app.MapControllerRoute(
-				name: "default",
-				pattern: "{controller=Home}/{action=Index}/{id?}");
-			//pattern: "{controller=Home}/{action=Homepage}/{id?}");
-			//pattern: "{controller=Registration}/{action=Register}/{id?}");
 
-			app.Run();
-		}
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            //pattern: "{controller=Home}/{action=Homepage}/{id?}");
+            //pattern: "{controller=Registration}/{action=Register}/{id?}");
+
+            app.Run();
+        }
 	}
 }

f11b51c7a61bb36513da9e812d847f54ecb369e1
anum zeb <anum.z95@simplilearn.com>
2023-02-13 22:00:04 +0500

    9:59 PM

--- Presentation/SLM.WebApp/Controllers/RegistrationController.cs
+++ Presentation/SLM.WebApp/Controllers/RegistrationController.cs
@@ -42,6 +42,79 @@ namespace SLM.WebApp.Controllers
 				return View();
 			}
 		}
+        // GET: AccountController
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
 
-	}
+        // GET: AccountController/Details/5
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+        // GET: AccountController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AccountController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AccountController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AccountController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AccountController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AccountController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
 }

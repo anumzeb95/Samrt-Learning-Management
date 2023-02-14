6490f21f5382c7082fd34832bc66c24e9bb1e2a8
anum zeb <anum.z95@simplilearn.com>
2023-02-14 20:26:41 +0500

    8:26PM

--- Repositories/SLM.Data/Migrations/20230214152321_co-lec.Designer.cs
+++ Repositories/SLM.Data/Migrations/20230214152321_co-lec.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SLM.Data;
 
@@ -11,9 +12,10 @@ using SLM.Data;
 namespace SLM.Data.Migrations
 {
     [DbContext(typeof(SLManagementDbContext))]
-    partial class SLManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230214152321_co-lec")]
+    partial class colec
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Repositories/SLM.Data/Migrations/20230214152321_co-lec.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SLM.Data.Migrations
+{
+    public partial class colec : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Leacture_Courses_CoursesId",
+                table: "Leacture");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Leacture_CoursesId",
+                table: "Leacture");
+
+            migrationBuilder.DropColumn(
+                name: "CoursesId",
+                table: "Leacture");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CoursesId",
+                table: "Courses",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Courses",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Leacture_CourseId",
+                table: "Leacture",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_CoursesId",
+                table: "Courses",
+                column: "CoursesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_UserId",
+                table: "Courses",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Courses_CoursesId",
+                table: "Courses",
+                column: "CoursesId",
+                principalTable: "Courses",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_User_UserId",
+                table: "Courses",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Leacture_Courses_CourseId",
+                table: "Leacture",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Courses_CoursesId",
+                table: "Courses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_User_UserId",
+                table: "Courses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Leacture_Courses_CourseId",
+                table: "Leacture");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Leacture_CourseId",
+                table: "Leacture");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_CoursesId",
+                table: "Courses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_UserId",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "CoursesId",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Courses");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CoursesId",
+                table: "Leacture",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Leacture_CoursesId",
+                table: "Leacture",
+                column: "CoursesId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Leacture_Courses_CoursesId",
+                table: "Leacture",
+                column: "CoursesId",
+                principalTable: "Courses",
+                principalColumn: "Id");
+        }
+    }
+}

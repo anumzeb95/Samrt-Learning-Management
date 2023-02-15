cb1f270c6a95bbfd0ce98711901d4bd8d9acf12a
anum zeb <anum.z95@simplilearn.com>
2023-02-15 16:51:19 +0500

    4:51 PM

---
+++ Presentation/SLM.WebApp/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SLM.Bussines.Interfaces;
+using SLM.WebApp.Models;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+
+namespace SLM.WebApp.Controllers
+{
+	//[Authorize]
+	public class AccountController : Controller
+	{
+		//private readonly ILogger<HomeController> _logger;
+        private readonly IUserService _userService;
+        public AccountController(IUserService userService)
+
+        {
+            _userService = userService;
+        }
+      
+        public IActionResult Dashboard()
+		{
+			return View();
+		}
+
+
+
+        public IActionResult Profile()
+		{
+			return View();
+		}
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
--- Presentation/SLM.WebApp/Controllers/RegistrationController.cs
+++ Presentation/SLM.WebApp/Controllers/RegistrationController.cs
@@ -83,6 +83,11 @@ namespace SLM.WebApp.Controllers
 
         }
 
+
+
+
+
+        [HttpPost]
         public async Task<IActionResult> Logout()
         {
             await this.HttpContext.SignOutAsync();
@@ -94,79 +99,7 @@ namespace SLM.WebApp.Controllers
 
 
 
-        // GET: AccountController
-        public ActionResult Dashboard()
-        {
-            return View();
-        }
 
-        // GET: AccountController/Details/5
-        public ActionResult Profile()
-        {
-            return View();
-        }
-
-        // GET: AccountController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: AccountController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: AccountController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: AccountController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: AccountController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: AccountController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+      
     }
 }

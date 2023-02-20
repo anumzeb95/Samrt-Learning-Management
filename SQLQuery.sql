923eb14079a3c45ee29a4b2a02605cad7020d133
anum zeb <anum.z95@simplilearn.com>
2023-02-20 12:23:23 +0500

    12:23 PM

--- SQLQuery.sql
+++
@@ -1,8 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [Id]
-      ,[Name]
-      ,[Duration]
-	  ,[Teacher]
-      ,[Description]
-      
-  FROM [SLMSystem].[dbo].[Courses]
